3ef1e6d24dfeed4f794d8c84ff30b54a8f1a4629
ryanburgess <ryanb@netflix.com>
2015-04-25 15:19:47 -0700

    create placeholder modules, add gulp

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp'),
+  minifyCSS = require('gulp-minify-css'),
+  sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+  return gulp.src('sass/**/*.scss')
+    .pipe(sass())
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('sass/**/*.scss', ['css']);
+});
+
+gulp.task('default', ['watch']);
