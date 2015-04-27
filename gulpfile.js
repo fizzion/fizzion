99e9b5efe611ef38cdedf627332f36003dbd8ebf
ryanburgess <ryanb@netflix.com>
2015-04-26 20:32:34 -0700

    create circular preloader

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ gulp.task('sass', function () {
 });
 
 gulp.task('watch', function() {
-  gulp.watch('sass/**/*.scss', ['css']);
+  gulp.watch('sass/**/*.scss', ['sass']);
 });
 
 gulp.task('default', ['watch']);
